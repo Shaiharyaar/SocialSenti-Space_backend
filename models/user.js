292270abb34dc0c92232fb927a9f2575c091b2fa
Shaiharyaar Ahmad <shaiharyaar1998@github.com>
2020-08-18 20:40:25 +0500

    hello

---
+++ models/user.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var passportLocalMongoose = require("passport-local-mongoose");
+var userSchema = new Schema({
+  email: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  firstname: {
+    type: String,
+  },
+  lastname: {
+    type: String,
+  },
+  admin: {
+    type: Boolean,
+    required: false,
+  },
+  image: {
+    type: String,
+  },
+});
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", userSchema);
